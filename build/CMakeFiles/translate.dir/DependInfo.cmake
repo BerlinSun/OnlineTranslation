63f3662cd89cddc653de11bb3506925eb651e48e
BerlinSun <bolinsun@outlook.com>
2014-07-27 16:23:06 +0800

    Minor fix, add some constants, share my dict key at this edition, add the build dict.

---
+++ build/CMakeFiles/translate.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/berlin/Documents/OnlineTranslation/translate.cc" "/home/berlin/Documents/OnlineTranslation/build/CMakeFiles/translate.dir/translate.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/berlin/Documents/jsoncpp/include"
+  "/home/berlin/Documents/curl-7.37.1/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/translate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/translate.dir/translate.cc.o"
+  "translate.pdb"
+  "translate"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/translate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
